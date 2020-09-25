8600f7bfa5f9c40041b57b0df91ac706731a083f
shahirk48 <42793050+shahirk48@users.noreply.github.com>
2020-09-25 19:54:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function loadText() {
     let xhr = new XMLHttpRequest();
 
-    xhr.open('GET','sample.txt',true);
+    xhr.open('GET','https://github.com/shahirk48/ajax-test-one/blob/master/sample.txt',true);
     
     xhr.onload = function() {
         if (this.status == 200){
